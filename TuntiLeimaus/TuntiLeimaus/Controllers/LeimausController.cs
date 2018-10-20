2165d518e2ceca1a1a284383018e0d1e7f35532c
Ahokas Maria Katariina <maria.ahokas@stud.pointcollege.fi>
2018-10-20 22:10:38 +0300

    New View, Layout, add images

---
+++ TuntiLeimaus/TuntiLeimaus/Controllers/LeimausController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TuntiLeimaus.Controllers
+{
+    public class LeimausController : Controller
+    {
+        // GET: Leimaus
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
