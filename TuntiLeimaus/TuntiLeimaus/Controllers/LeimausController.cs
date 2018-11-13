bd59c3e066bbaf5b4a4d940aef47e31ec5bd81ad
Jalmari Talvipuro <jalmari.talvipuro@stud.pointcollege.fi>
2018-11-13 10:48:40 +0200

    test

---
+++ TuntiLeimaus/TuntiLeimaus/Controllers/LeimausController.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TuntiLeimaus.Models;
+
+namespace TuntiLeimaus.Controllers
+{
+    public class LeimausController : Controller
+    {
+        // GET: Leimaus
+        public ActionResult Index()
+        {
+           return View();
+        }
+        public JsonResult GetList()
+        {
+
+            //Tämä malli antaa enemmän mahdollisuuksia
+            TuntiLeimausEntities entities = new TuntiLeimausEntities();
+
+            var model = (from p in entities.Leimaus
+                         select new
+                         {
+                             p.OpiskelijaID,
+                             p.LuokkahuoneID,
+                             p.Paivamaara,
+                             p.Sisaan,
+                             p.Ulos
+
+                         }).ToList();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+
+            Response.Expires = -1;
+            Response.CacheControl = "no-cache";
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetSingLeimaus(int id)
+        {
+
+            //Tämä malli antaa enemmän mahdollisuuksia
+            TuntiLeimausEntities entities = new TuntiLeimausEntities();
+            //List<Customer> model = entities.Customers.ToList();
+            var model = (from p in entities.Leimaus
+                         where p.OpiskelijaID == id
+                         select new
+                         {
+                             p.OpiskelijaID,
+                             p.LuokkahuoneID,
+                             p.Paivamaara,
+                             p.Sisaan,
+                             p.Ulos
+                         }).FirstOrDefault();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Update(Leimaus pro)
+        {
+            TuntiLeimausEntities entities = new TuntiLeimausEntities();
+            int id = pro.OpiskelijaID;
+
+            bool OK = false;
+
+            if (pro.OpiskelijaID == 0)
+
+            {
+
+                // kyseessä on uuden asiakkaan lisääminen, kopioidaan kentät
+                Leimaus dbItem = new Leimaus()
+                {
+                    OpiskelijaID = pro.OpiskelijaID,
+                    LuokkahuoneID = pro.LuokkahuoneID,
+                    Paivamaara = pro.Paivamaara,
+                    Sisaan = pro.Sisaan,
+                    Ulos = pro.Ulos
+
+                };
+
+                // tallennus tietokantaan
+                entities.Leimaus.Add(dbItem);
+                entities.SaveChanges();
+                OK = true;
+            }
+
+            entities.Dispose();
+            return Json(OK, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult Index2()
+        {
+            return View();
+
+        }
+
+    }
+}
+    }
+}
---
+++ TuntiLeimaus/TuntiLeimaus/Controllers/OpettajaController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TuntiLeimaus.Models;
+using Newtonsoft.Json;
+
+namespace TuntiLeimaus.Controllers
+{
+    public class OpettajaController : Controller
+    {
+        // GET: Opettaja
+        public ActionResult Index()
+        {
+            ScrumEntities entities = new ScrumEntities();
+            List<Tuntiraportti> model = entities.Tuntiraportti.ToList();
+            entities.Dispose();
+
+            return View(model);
+        }
+
+        public JsonResult GetList()
+        {
+            ScrumEntities entities = new ScrumEntities();
+           
+
+            var model = (from c in entities.Tuntiraportti
+                         select new
+                         {
+                             OpiskelijaID = c.OpiskelijaID,
+                             Etunimi = c.Etunimi,
+                             Sukunimi = c.Sukunimi,
+                             LuokkahuoneID = c.LuokkahuoneID,
+                             Sisään = c.Sisään,
+                             Ulos = c.Ulos
+                         }).ToList();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+            //välimuistin hallinta
+            Response.Expires = -1;
+            Response.CacheControl = "no-cache";
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetSingleTuntiraportti(string id)
+        {
+            ScrumEntities entities = new ScrumEntities();
+            int opiskelijaid = int.Parse(id);
+            var model = (from c in entities.Tuntiraportti
+                         where c.OpiskelijaID == opiskelijaid
+                         select new
+                         {
+                             OpiskelijaID = c.OpiskelijaID,
+                             Etunimi = c.Etunimi,
+                             Sukunimi = c.Sukunimi,
+                             LuokkahuoneID = c.LuokkahuoneID,
+                             Sisään = c.Sisään,
+                             Ulos = c.Ulos
+                         }).FirstOrDefault();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Update(Tuntiraportti tunt)
+        {
+
+            ScrumEntities entities = new ScrumEntities();
+
+            //oletetaan että tallennusoperaatio ei onnistu
+            bool OK = false;
+
+            // onko kyseessä muokkaus vai uuden lisääminen?
+            //if (id == "(uusi)")
+            if (tunt.OpiskelijaID == 0)
+            //if (id == null)
+            {
+                // kyseessä on uuden asiakkaan lisääminen, kopioidaan kentät
+                Tuntiraportti dbItem = new Tuntiraportti()
+                {
+                    //HenkiloID = henk.HenkiloID,
+                    Etunimi = tunt.Etunimi,
+                    Sukunimi = tunt.Sukunimi,
+                    LuokkahuoneID = tunt.LuokkahuoneID,
+                    Sisään = tunt.Sisään,
+                    Ulos = tunt.Ulos
+                };
+
+                // tallennus tietokantaan
+                entities.Tuntiraportti.Add(dbItem);
+                entities.SaveChanges();
+                OK = true;
+            }
+            else
+            {
+                //haetaan id:n perusteella rivi SQL tietokannasta
+                Tuntiraportti dbItem = (from h in entities.Tuntiraportti
+                                   where h.OpiskelijaID == tunt.OpiskelijaID
+                                   select h).FirstOrDefault(); //haetaan vain yhden henkilön tiedot
+
+                //jos tiedot löytyvät eli ei ole null
+                if (dbItem != null)
+                {
+                    //dbItem.HenkiloID = henk.HenkiloID;  //tätä ei käytetä
+                    dbItem.Etunimi = tunt.Etunimi;
+                    dbItem.Sukunimi = tunt.Sukunimi;
+                    dbItem.LuokkahuoneID = tunt.LuokkahuoneID;
+                    dbItem.Sisään = tunt.Sisään;
+                    dbItem.Ulos = tunt.Ulos;
+
+                    // tallennus SQL tietokantaan
+                    entities.SaveChanges();
+
+                    //jos tallennus onnistuu
+                    OK = true;
+                }
+            }
+            //entiteettiolion vapauttaminen
+            entities.Dispose();
+
+            // palautetaan 'json' muodossa
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            ScrumEntities entities = new ScrumEntities();
+
+            //etsitään id:n perusteella henkilöt kannasta
+            int opiskelijaid = int.Parse(id);
+            bool OK = false;
+            Tuntiraportti dbItem = (from h in entities.Tuntiraportti
+                               where h.OpiskelijaID == opiskelijaid
+                               select h).FirstOrDefault();
+
+            if (dbItem != null)
+            {
+                //tietokannasta poisto
+                entities.Tuntiraportti.Remove(dbItem);
+                //tallennus SQL tietokantaan
+                entities.SaveChanges();
+
+                //jos tallennus onnistuu
+                OK = true;
+            }
+
+            entities.Dispose();
+
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
